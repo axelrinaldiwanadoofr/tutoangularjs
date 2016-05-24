146d012a8d0c2f4bb38e8bc9307ee240325a7094
axel <axel.rinaldi@wanadoo.fr>
2016-05-24 19:39:05 +0200

    Ajout du système de routage, de la directive ng-view et de la vue vwListeDePersonnes.

--- js/app.js
+++ js/app.js
@@ -5,8 +5,35 @@
 * Appel de la méthode "module" de l'objet Angular référencé par la variable globale
 * angular. Cette méthode crée un objet associée à notre application (cf directive ng-app placé dans la 
 * balise <html> du code HTML) et renvoie sa référence que l'on copie dans la variable app
+* 
+* Demande à charger le module "ngRoute" de routage de page
 */
-var app = angular.module( 'MonApp', [] ) ;
+var app = angular.module( 'MonApp', ["ngRoute"] ) ;
+
+/*
+ * 
+ * L'appel de la méthode "config" de AngularJS permet de paramétrer le service de routage des pages d'AngularJS
+ * L'argument $routeProvider injecté dans la fonction contient la référence à ce service. Ce dernier possède la 
+ * méthode "when" permettant de créer des entrés dans le système de routage de page (Ce système permets de gérer
+ * les liens hyper-text qui sont placés dans le code HTML). Chaque entrée contient un chemin de navigation et un objet
+ * dont les attributs renseignent le système de routage sur les taches à réaliser. 
+ * 
+ * La première entrée correspond à la liste des personne et charge de manière asynchrone le contenu du fichier
+ * "vwListeDePersonnes.html" qui viendra en remplacement du contenu de la balise <article> contenant la directive
+ * "ng-view"
+ * 
+ *  L'appel de la méthode "otherwise" permet de préciser quelle vue est utilisée par défaut.
+ */
+app.config( ['$routeProvider',function( $routeProvider)
+{
+    // Route pour accéder à la liste des personnes. Utilise la vue stockée dans le fichier vwListeDePersonnes.html qui
+    // viendra en remplacement de la directive ng-view
+    $routeProvider.when( '/list', {
+        templateUrl: 'view/vwListeDePersonnes.html'
+        });
+        
+    $routeProvider.otherwise( {redirectTo: '/list'} ) ;
+}]);
 
 /*
  * Création du controleur de donnée "LesPersonnesController" qui crée un modèle de données
@@ -16,7 +43,7 @@ var app = angular.module( 'MonApp', [] ) ;
  * tableau dont chaque case référence un objet contenant les données d'une personne.   
  *    
  */
-app.controller( "PersonneController", function( $scope )
+app.controller( "PersonneController", ["$scope",function( $scope )
 {
     $scope.lesPersonnes = [
         {id: 1, nom: "Meyer", prenom: "Paul" },
@@ -24,6 +51,6 @@ app.controller( "PersonneController", function( $scope )
         {id: 3, nom: "Dupond", prenom: "Pauline" },
         {id: 4, nom: "Duschmol", prenom: "Robert" }
     ];
-});
+}]);
 
 
