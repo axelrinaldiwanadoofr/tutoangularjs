40a1b730d4994b76939c536b27ca0d42134141cd
axel <axel.rinaldi@wanadoo.fr>
2016-05-30 18:00:40 +0200

    Création d'un factory générant un provider pour WebSql. Utilisation du provider dans le controleur PersonneController

--- js/app.js
+++ js/app.js
@@ -9,96 +9,63 @@
 var app = angular.module( 'MonApp', [] ) ;
 
 
-
-
-/*
- * Test d'utilisation de WebSQL: 
- * - ouverture/creation d'une base de données
- * - creation de la table Personnes( id, nom, prenom ) 
- * - insertion de quelques occurences
- */
-
-// Ouvre ou crée la base de données locale de type WEBSQL de nom MaBD
-// version n°1 et d'une taille de 1Mo
-var db = openDatabase( "MaBD", "1", "Ceci est un commentaire", 1000000 ) ;
-
-if( db )
+// Enregistre une fabrique au sein d'Angular qui créera et retournera 
+// une instance d'un provider WebSql 
+app.factory( "webSql", [function() 
 {
-    //
-    // Cree la table Personnes
-    //
+    // Cree un objet vide
+    var provider = {} ;
     
-    // Requete de creation de la table Personnes
-    var sql_ct = "CREATE TABLE IF NOT EXISTS Personnes( id int, nom text, prenom text )" ;
+    // Ouvre ou crée la base de données locale de type WEBSQL de nom MaBD
+    // version n°1 et d'une taille de 1Mo
+    provider.db = openDatabase( "MaBD", "1", "Ceci est un commentaire", 1000000 ) ;
     
-    // Ouvre un cycle de transaction dans WEBSQL (Traitement asynchrone)
-    db.transaction( function(t) 
+    // Methode exec( sql, bindings, onSuccesFct( provider, results ) )
+    // sql: Requete SQL
+    // bindings: Tableau de valeurs
+    // onSuccesFct( provider, results ): Référence du callback à exécuter en cas de succes
+    //      provider: Référence du provider
+    //      results: Référence de l'objet contenant le résultat de la requete
+    provider.exec = function( sql, bindings, onSuccesFct )
     {
-        // Envoie la requete SQL à WEBSQL
-        t.executeSql( sql_ct, [], 
-            function( t, results )
-            {
-                // Traitement du résultat en cas de succes
-                console.log( "Table Personnes créée") ;
-            },
-            function( t, error )
-            {
-                // Traitement d'erreur en cas d'echec
-                alert( "Erreur code " + error.code + " " + error.message ) ;
-            }
-        );
-    });                                        
-}
-
-if( db )
-{
-    // 
-    // Compte le nombre de personnes dans la table Personnes
-    //
-    db.transaction( function(t) 
-    {
-        // Envoie la requete SQL à WEBSQL
-        t.executeSql( "select count(*) as nb from Personnes", [], 
-            function( t, results )
-            {
-                // Traitement du résultat en cas de succes
-                // Si le nombre de personne est 0 alors insère un enregistrement 
-                // si non pas.
-                if( results.rows[0].nb == 0 )
+        // Stocke dans une variable de closure la référence de l'objet servant
+        // à appeler la méthode
+        var $this = this ;
+        
+        // Ouvre un cycle de transaction dans WEBSQL (Traitement asynchrone)
+        this.db.transaction( function( transaction ) 
+        {
+            // Envoie la requete SQL à WEBSQL
+            transaction.executeSql( sql, bindings, 
+                function( transaction, results )
                 {
-                    // Requete d'insertion
-                    var sql_ins = "INSERT INTO Personnes( id, nom, prenom ) values( ?, ?, ? )" ;
-
-                    // Tableau de valeur pour le data-binding
-                    var values = [1, "DUPOND", "Charles"] ;
-    
-                    // Envoie la requete SQL à WEBSQL
-                    t.executeSql( sql_ins, values, 
-                        function( t, results )
-                        {
-                            // Traitement du résultat en cas de succes
-                            console.log( "Insertion d'un enregistrement dans Personnes" ) ;
-                        },
-                        function( t, error )
-                        {
-                            // Traitement d'erreur en cas d'echec
-                            alert( "Erreur code " + error.code + " " + error.message ) ;
-                        }
-                    ) ;
+                    if( onSuccesFct ) onSuccesFct( $this, results ) ;
+                },
+                function( transaction, error )
+                {
+                    // Traitement d'erreur en cas d'echec
+                    alert( "Erreur code " + error.code + " " + error.message ) ;
                 }
-                else console.log( "La table Personnes contient déjà des données, pas d'insertion effectuée" ) ;
-            },
-            function( t, error )
-            {
-                // Traitement d'erreur en cas d'echec
-                alert( "Erreur code " + error.code + " " + error.message ) ;
-            }
-        );
-    });                                        
-}    
-
-
-
+            );
+        });                                                
+    };
+    
+    // Intialsaition sur la BD
+    // Cree la table Personnes
+    provider.exec( "CREATE TABLE IF NOT EXISTS Personnes( id int, nom text, prenom text )" ) ;
+    // Insere une personne si la table est vide
+    provider.exec( "select count(*) as nb from Personnes", [], function( provider, results)
+    {
+        if( results.rows[0].nb == 0 )
+        {
+            // Insere la personne si la table Personnes ne contient pas d'occurence
+            provider.exec( "INSERT INTO Personnes( id, nom, prenom ) values( ?, ?, ? )", [1, "DUPOND", "Charles"] ) ;
+        }
+    }) ;
+    
+    // Retourne l'objet singleton
+    return provider ;
+}]);
 
 /*
  * Création du controleur de donnée "LesPersonnesController" qui crée un modèle de données
@@ -106,43 +73,32 @@ if( db )
  * 
  * Le controleur ajoute cette fois au scope l'attribut "lesPersonnes" dans lequel 
  * est copié la référence du tableau lesPersonnes chargé ci-dessus  
+ * 
+ * Ajout par injection de l'argument webSql faisant référence au provider WebSql
+ * 
  */
-app.controller( "PersonneController", ["$scope",function( $scope )
+app.controller( "PersonneController", ["$scope","webSql",function( $scope, webSql )
 {
     // Création d'un tableau vide
     $scope.lesPersonnes = [] ;
     
-    if( db )
+    // 
+    // Recupere la liste des personnes
+    //
+    webSql.exec( "select * from Personnes", [], function( provider, results )
     {
-        // 
-        // Recupere la liste des personnes
-        //
-        db.transaction( function(t) 
-        {
-            // Envoie la requete SQL à WEBSQL
-            t.executeSql( "select * from Personnes", [], 
-                function( t, results )
-                {
-                    // Copie les références des personnes dans le tableau lesPersonnes
-                    // Ici on fait une copie car le tableau results.rows n'est utilisable
-                    // directement pour un modèle de donnée.
+        // Copie les références des personnes dans le tableau lesPersonnes
+        // Ici on fait une copie car le tableau results.rows n'est utilisable
+        // directement pour un modèle de donnée.
 
-                    for( var i=0; i< results.rows.length; i++ )
-                    {
-                        $scope.lesPersonnes.push( results.rows[i] ) ;
-                    }
-                    
-                    // Demande à rafraichir les vues basées sur ce scope.
-                    $scope.$digest() ;
-                },
-                function( t, error )
-                {
-                    // Traitement d'erreur en cas d'echec
-                    alert( "Erreur code " + error.code + " " + error.message ) ;
-                }
-            );
-        });                                        
-    }    
+        for( var i=0; i< results.rows.length; i++ )
+        {
+            $scope.lesPersonnes.push( results.rows[i] ) ;
+        }
+        
+        // Rafraichi la vue
+        $scope.$digest() ;
+    }) ;
 }]);
 
 
