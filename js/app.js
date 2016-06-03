0b85b698dd28b4e871df10a6fe3d9fed73daa347
axel <axel.rinaldi@wanadoo.fr>
2016-06-03 22:50:57 +0200

    Ajout dynamique d'attributs

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@
 // Création d'un objet vide et copie de sa référence dans la variable p
 var p = {} ;
 
-
+// Ajout dynamique d'attributs à l'objet
+p.nom = "MEYER" ;
+p.prenom = "Paul" ;
+p.age = 24 ;
 
 
