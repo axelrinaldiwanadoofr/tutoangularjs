77d8a1a8cfd7310ee51636ad8edf66e62348ac75
Axel RINALDI <axel.rinaldi@wanadoo.fr>
2016-06-27 16:23:15 +0200

    Modifie le diagramme objet

--- js/app.js
+++ js/app.js
@@ -6,3 +6,42 @@ setTimeout( function()
 {
     alert( "T: 1s") ;
 }, 1000 ) ;
+
+
+function Toto( age )
+{
+    age++ ;
+}
+
+Toto( 23 ) ;
+
+var f = Toto ;
+
+f( 23 ) ;
+
+function Titi( fct )
+{
+    fct( 27 ) ;
+}
+
+Titi( Toto ) ;
+
+Titi( f ) ;
+
+Titi( function( valeur)
+{
+    console.log( valeur + 1) ;
+});
+
+function Tata( valeur )
+{
+    var ageDuCapitaine = valeur + 1 ;
+    
+    setTimeout( function()
+    {
+        alert( ageDuCapitaine ) ;
+    }, 1000 ) ;
+}
+
+Tata( 36 ) ;
+Tata( 57 ) ;
